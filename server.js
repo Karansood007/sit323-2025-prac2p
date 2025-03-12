9e17aeaa9de57ba9edf621ddce6c536abdea76c9
Karan sood <141621576+Karansood007@users.noreply.github.com>
2025-03-13 03:27:01 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,16 +2,16 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+// Log when the server is starting
+console.log("Server is starting...");
+
+// Serve a simple HTML page
 app.get('/', (req, res) => {
+    console.log("Received a request!");
     res.send('<h1>Hello, welcome to my website!</h1>');
 });
 
-// Error handling middleware
-app.use((err, req, res, next) => {
-    console.error('Something went wrong:', err);
-    res.status(500).send('Something went wrong!');
-});
-
+// Start the server
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
